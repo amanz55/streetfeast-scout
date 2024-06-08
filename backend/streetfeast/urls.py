0c9dd05481152d4574fc438a9420e822374e2f0c
amanz55 <amanuelalemayehu449@gmail.com>
2024-06-08 12:24:22 +0300

    feat: added a test workflow

--- backend/streetfeast/urls.py
+++ backend/streetfeast/urls.py
@@ -21,3 +21,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
