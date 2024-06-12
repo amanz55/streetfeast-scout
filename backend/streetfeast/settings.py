3f8007e12d5fa61ff1497fad929769861d65c33a
amanz55 <amanuelalemayehu449@gmail.com>
2024-06-12 16:10:06 +0300

    feat: vercel deployment

--- backend/streetfeast/settings.py
+++ backend/streetfeast/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui\static")]
-STATIC_URL = "static/"
+# STATIC_URL = "static/"
 # STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
 
 # Default primary key field type
